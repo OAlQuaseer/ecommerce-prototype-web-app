374599d82078bb9568181a962be17035c2bd6233
Omar AlQuaseer <your0helper0tech@gmail.com>
2022-08-04 11:42:14 -0500

    Build the directory page.

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import './category-item.scss';
+
+
+const CategoryItem = ({ category: { title, imageUrl } }) => (
+    <div className="category-container">
+        <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+        }}></div>
+        <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+        </div>
+    </div>
+)
+
+
+export default CategoryItem
