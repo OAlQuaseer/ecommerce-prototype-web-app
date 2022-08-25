86591bd3893f356b982a11075c97e628806da497
Omar AlQuaseer <your0helper0tech@gmail.com>
2022-08-25 15:54:29 -0500

    Added links to the home page.

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,22 @@
 import './directory-item.styles.scss';
+import { useNavigate } from 'react-router-dom';
 
 
-const DirectoryItem = ({ category: { title, imageUrl } }) => (
-    <div className="directory-item-container">
-        <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-        }}></div>
-        <div className="body">
-            <h2>{title}</h2>
-            <p>Shop now</p>
+const DirectoryItem = ({ category: { title, imageUrl, route } }) => {
+    const navigate = useNavigate();
+    const onClickHandler = () => navigate(route);
+    return (
+        <div className="directory-item-container" onClick={onClickHandler}>
+            <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+            }}></div>
+            <div className="body">
+                <h2>{title}</h2>
+                <p>Shop now</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 export default DirectoryItem
